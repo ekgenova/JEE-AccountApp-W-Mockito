dcd77a81bc58d9923fca71f07510ed3e82d4b3a1
Kate <e.genova@live.co.uk>
2018-07-17 16:42:13 +0100

    db test

---
+++ src/test/java/com/qa/persistence/repository/test/AccountDBRepositoryTest.java
@@ -0,0 +1,74 @@
+package com.qa.persistence.repository.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.qa.persistence.domain.Account;
+import com.qa.persistence.repository.AccountDBRepository;
+import com.qa.util.JSONUtil;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AccountDBRepositoryTest {
+
+	@InjectMocks
+	private AccountDBRepository repo;
+	
+	@Mock
+	private EntityManager manager;
+	
+	@Mock
+	private Query query;
+	
+	private JSONUtil util;
+	
+	private static final String MOCK_DATA_ARRAY = "[{\"firstName\":\"John\",\"secondName\":\"Doe\",\"accountNumber\":\"111111\"}]";
+
+	private static final String MOCK_OBJECT = "{\"firstName\":\"John\",\"secondName\":\"Doe\",\"accountNumber\":\"111111\"}";
+
+	@Before
+	public void setUp() {
+		repo.setManager(manager);
+		util = new JSONUtil();
+		repo.setUtil(util);
+	}
+	
+	@Test
+	public void testGetAllAccounts() {
+		Mockito.when(manager.createQuery(Mockito.anyString())).thenReturn(query);
+		List<Account> accounts = new ArrayList<Account>();
+		accounts.add(new Account("John", "Doe", "111111", null));
+		Mockito.when(query.getResultList()).thenReturn(accounts);
+		assertEquals(MOCK_DATA_ARRAY, repo.getAllAccounts());
+	}
+	
+	@Test
+	public void testCreateAccount() {
+		String reply = repo.createAccount(MOCK_OBJECT);
+		assertEquals(reply, "{\"message\": \"account has been sucessfully added\"}");
+	}
+	
+	@Test
+	public void testUpdateAccount() {
+		String reply = repo.updateAccount(1L, MOCK_OBJECT);
+		assertEquals(reply, "{\"message\": \"account sucessfully updated\"}");
+	}
+	
+	@Test
+	public void testDeleteAccount() {
+		String reply = repo.deleteAccount(1L);
+		assertEquals(reply, "{\"message\": \"account sucessfully deleted\"}");
+	}
+}
